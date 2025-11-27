03487a7e029d7c8bd2c4c6e6330cd06ebd6396af
Gabriel <gabriel.rodrigues.costa2001@gmail.com>
2025-11-26 21:53:36 -0300

    feat(favicon): Adicionar favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,22 @@ import type { Metadata } from "next";
 import "./globals.css";
 import StyledComponentsRegistry from "@/lib/registry";
 import { ThemeProvider } from "@/context/Context";
+
 export const metadata: Metadata = {
   title: "Dashboard",
   description: "Painel de vendas",
+  icons: {
+    icon: "/dash.png",
+  },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-BR">
       <body>
         <StyledComponentsRegistry>
           <ThemeProvider>{children}</ThemeProvider>
